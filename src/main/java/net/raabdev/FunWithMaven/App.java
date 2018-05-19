a2f4f5e7c14d240c413e2adb2840b595993b38f5
sraab123 <scraab1@gmail.com>
2018-05-19 19:08:39 -0400

    Initial Commit for FunWithMaven

---
+++ src/main/java/net/raabdev/FunWithMaven/App.java
@@ -0,0 +1,13 @@
+package net.raabdev.FunWithMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
